e2871006b94c8600a01737356d67b1ed67207440
marwan <marwan4125881@gmail.com>
2025-02-16 15:43:41 +0200

    navbar events

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -2,11 +2,29 @@ import "./Navbar.scss";
 import logo from '../../assets/logo.png';
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUtensils } from '@fortawesome/free-solid-svg-icons';
+import { faUtensils, faBarsStaggered } from '@fortawesome/free-solid-svg-icons';
 
 export default function Navbar(){
+    let nav = document.querySelector('.navbar');
+    nav?.addEventListener("click", (e)=>{
+        e.stopPropagation();
+    });
+    document.addEventListener("click",()=>{
+        if(!nav?.classList.contains('toggle')){
+            nav?.classList.add('toggle');
+        }
+        
+    })
+    function toggleNavbar(e:Event){
+        e.stopPropagation();
+        nav?.classList.remove('toggle');
+    }
 
-    return <div className="navbar">
+    return <div className="nav-container">
+        <div className="toggle-icon">
+        <FontAwesomeIcon icon={faBarsStaggered} onClick={(e)=> toggleNavbar(e)}/>
+        </div>
+        <div className="navbar toggle">
         <div className="image-container">
             <img src={logo} alt="recipe" />
         </div>
@@ -15,5 +33,6 @@ export default function Navbar(){
             <Link to="/"><FontAwesomeIcon icon={faUtensils} />Ingrediants</Link>
             <Link to="/"><FontAwesomeIcon icon={faUtensils} />Area</Link>
         </div>
+    </div>
     </div>
 }
